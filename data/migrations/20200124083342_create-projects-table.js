55a57cbd18aa6d0fa9b56034d7977a4dd7259900
Annette Odingo <odingoannette@gmail.com>
2020-01-24 08:54:34 +0000

    Create tables and define their structure in migration

--- data/migrations/20200124083342_create-projects-table.js
+++ data/migrations/20200124083342_create-projects-table.js
@@ -1,8 +1,58 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', tbl=>{
+      tbl.increments();
+      tbl.text('name', 128)
+        .notNullable();
+      tbl.text('description');
+      tbl.boolean('completed')
+        .notNullable()
+        .defaultTo(false);
+  })
+  .createTable('resources', tbl=>{
+    tbl.increments();
+    tbl.text('name', 128)
+      .notNullable();
+    tbl.text('description');
+  })
+  .createTable('tasks', tbl=>{
+    tbl.increments();
+    tbl.text('description')
+      .notNullable();
+    tbl.text('notes');
+    tbl.boolean('completed')
+        .notNullable()
+        .defaultTo(false);
+    tbl.integer('projectId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('schemes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+  })
+ .createTable('resources_projects', tbl=>{
+    tbl.increments();
+    tbl.integer('projectId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    tbl.integer('resourcesId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('resources')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources');
 };
